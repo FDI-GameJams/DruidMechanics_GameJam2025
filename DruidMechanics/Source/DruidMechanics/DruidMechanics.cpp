cdfc9812bf2c31f02295ae9c8a2f3d65478a1dae
Ivan Franco <ifrancoh@ucsc.edu>
2025-08-03 14:04:47 -0700

    added C++ solution to project

---
+++ DruidMechanics/Source/DruidMechanics/DruidMechanics.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DruidMechanics.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DruidMechanics, "DruidMechanics" );
