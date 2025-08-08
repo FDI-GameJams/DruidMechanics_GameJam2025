ea78efc6e9c54f29cafd70f3fc55b5d52c8526dc
Ivan Franco <ifrancoh@ucsc.edu>
2025-08-07 19:48:36 -0700

    finished implementing item base C++ class and started working on second level (multipath and pickable items tutorial)

--- DruidMechanics/Source/DruidMechanics/Private/Interactables/ItemBase.cpp
+++ DruidMechanics/Source/DruidMechanics/Private/Interactables/ItemBase.cpp
@@ -9,6 +9,15 @@ AItemBase::AItemBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Initialize the default scene root component and set it as the root component
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Default Scene Root"));
+	SetRootComponent(DefaultSceneRoot);
+
+	// Initialize the item mesh component and attach it to the root component
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Item Mesh"));
+	ItemMesh->SetupAttachment(RootComponent);
+
+
 }
 
 // Called when the game starts or when spawned
--- DruidMechanics/Source/DruidMechanics/Public/Interactables/ItemBase.h
+++ DruidMechanics/Source/DruidMechanics/Public/Interactables/ItemBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Enumerations/EItemType.h"
 #include "ItemBase.generated.h"
 
 UCLASS()
@@ -17,6 +18,18 @@ private:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties")
+	USceneComponent* DefaultSceneRoot{ nullptr };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties")
+	UStaticMeshComponent* ItemMesh{ nullptr };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties")
+	TEnumAsByte<EItemType> ItemType{ EItemType::NONE };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties")
+	int ItemQuantity{ 1 };
 
 public:	
 	// Sets default values for this actor's properties
