7805c8b589173be7708632d80c29155379384184
Ivan Franco <ifrancoh@ucsc.edu>
2025-08-07 17:57:12 -0700

    added item base C++ class for parent class of interactable objects

---
+++ DruidMechanics/Source/DruidMechanics/Public/Interactables/ItemBase.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBase.generated.h"
+
+UCLASS()
+class DRUIDMECHANICS_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Sets default values for this actor's properties
+	AItemBase();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
